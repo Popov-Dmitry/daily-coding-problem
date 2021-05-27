e9cb3fa0a56e8368561f734863e363697767ebb1
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-05-28 00:11:00 +0700

    Solved problem #5

---
+++ problems/p1_100/p5___Jane_Street/Solution.java
@@ -0,0 +1,20 @@
+package p1_100.p5___Jane_Street;
+
+public class Solution {
+    public static void main(String[] args) {
+        Cons<Integer, String> cons = new Cons<>(1, "abc");
+        System.out.println(cons.car(cons));
+        System.out.println(cons.cdr(cons));
+    }
+
+    private record Cons<T1, T2>(T1 a, T2 b) {
+
+        public Object car(Cons cons) {
+            return cons.a;
+        }
+
+        public Object cdr(Cons cons) {
+            return cons.b;
+        }
+    }
+}
